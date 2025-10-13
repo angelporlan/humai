0a375d736aa776be61e49698b369b8ac27b35bdc
angelporlan <angeelporlan4@gmail.com>
2025-10-13 20:01:57 +0200

    Create ReactionFactory to define default state for Reaction model

---
+++ backend/database/factories/ReactionFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reaction>
+ */
+class ReactionFactory extends Factory
+{
+    protected $model = \App\Models\Reaction::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $types = ['like','love','haha','wow','sad','angry'];
+
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'reactionable_id' => \App\Models\Post::factory(),
+            'reactionable_type' => \App\Models\Post::class,
+            'type' => $this->faker->randomElement($types),
+            'created_at' => $this->faker->dateTimeBetween('-6 months', 'now'),
+            'updated_at' => now(),
+        ];
+    }
+}
