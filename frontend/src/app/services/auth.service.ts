8c6eb25bf186b4688b8fffdba265c95d20615dfc
angelporlan <angeelporlan4@gmail.com>
2025-10-16 22:51:26 +0200

    Add AuthService and its unit tests for user authentication functionality

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+
+  register(username: string, email: string, password: string, password_confirmation: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, { username, email, password, password_confirmation });
+  }
+
+  logout(): Observable<any> {
+    const token = localStorage.getItem('token');
+      return this.http.post(`${this.apiUrl}/logout`,
+      {},  
+      {
+        headers: new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+        })
+      }
+    );
+  }
+
+  isLoggedIn(token: string): boolean {
+    return !!token;
+  }
+}
