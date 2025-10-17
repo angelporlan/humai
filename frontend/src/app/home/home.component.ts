df48ff40926484d893d485e62b031f42a949a156
angelporlan <angeelporlan4@gmail.com>
2025-10-17 22:11:56 +0200

    Add Feed display functionality to HomeComponent with feed retrieval and rendering

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,21 +1,41 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { FeedService } from '../services/feed.service';
+import { FeedItemComponent } from "../components/feed-item/feed-item.component";
+import { NgFor } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    imports: [FeedItemComponent, NgFor],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent {
-    constructor(private authService: AuthService, private router: Router) { }
+    feed: any[] = [];
+    constructor(private authService: AuthService, private router: Router, private feedService: FeedService) { }
+
+    ngOnInit() {
+        this.getFeed();
+    }
 
     logout() {
         this.authService.logout().subscribe({
             next: () => {
                 localStorage.removeItem('token');
                 this.router.navigate(['/auth/login']);
+            },
+            error: (err) => console.error(err)
+        });
+    }
+
+    getFeed() {
+        console.log('getFeed');
+        this.feedService.getFeed().subscribe({
+            next: (res) => {
+                console.log(res);
+                this.feed = Array.isArray(res.data) ? res.data : [res.data];
+                console.log(this.feed);
             },
             error: (err) => console.error(err)
         });
