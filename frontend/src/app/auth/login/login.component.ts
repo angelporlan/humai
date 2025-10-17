bd7dcf5979a03be2a8793e30ccb869933867a5a6
angelporlan <angeelporlan4@gmail.com>
2025-10-17 22:11:42 +0200

    Add LoginComponent with template, styles, and basic authentication functionality

---
+++ frontend/src/app/auth/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email = 'angel@example.com';
+  password = 'secret123';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/home']);
+      },
+      error: (err) => console.error(err)
+    });
+  }
+}
