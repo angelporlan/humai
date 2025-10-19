3ffb6884f5a73c731189bd9972bfc250459262a1
angelporlan <angeelporlan4@gmail.com>
2025-10-19 12:59:49 +0200

    Refactor routing to include LayoutComponent and nested routes for home and profile

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,17 +1,48 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './views/layout/layout.component';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'auth/login',
-        pathMatch: 'full'
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            },
+            {
+                path: 'home',
+                loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+            },
+            {
+                path: 'profile',
+                loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+            }
+        ]
     },
     {
         path: 'auth',
         loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
     },
-    {
-        path: 'home',
-        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-    }
 ];
+
+// export const routes: Routes = [
+//     {
+//         path: '',
+//         redirectTo: 'home',
+//         pathMatch: 'full'
+//     },
+//     {
+//         path: 'auth',
+//         loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+//     },
+//     {
+//         path: 'home',
+//         loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+//     },
+//     {
+//         path: 'profile',
+//         loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+//     }
+// ];
