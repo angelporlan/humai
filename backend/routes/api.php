fb5b22689f5940a7daa1fa0ba4a0f9f509258ee2
angelporlan <angeelporlan4@gmail.com>
2025-10-20 20:37:34 +0200

    Add route for getPostsOfAUser in authenticated group

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
     Route::get('/getInfoOfAUsername', [AuthController::class, 'getInfoOfAUsername']);
+    Route::get('/getPostsOfAUser', [PostController::class, 'getPostsOfAUser']);
 });
 
 // borrar para el commit
