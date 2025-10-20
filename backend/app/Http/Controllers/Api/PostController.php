924eb8c1ea7ff64da4b569f7a7e6d5c4eee5bf65
angelporlan <angeelporlan4@gmail.com>
2025-10-20 20:37:27 +0200

    Refactor PostController to improve getPostsOfAUser method and enhance feed functionality

--- backend/app/Http/Controllers/Api/PostController.php
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
+use App\Models\Post;
+use App\Models\User;
 use App\Services\FeedService;
+use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
@@ -25,7 +28,21 @@ class PostController extends Controller
         $perPage = $request->get('per_page', 10);
 
         $posts = $this->feedService->getFeed($user, $perPage);
-        // $posts = $this->feedService->feed($user, $perPage);
+
+        return PostResource::collection($posts);
+    }
+
+    public function getPostsOfAUser(Request $request)
+    {
+        $username = $request->get('username');
+        //obtener el usuario por el username
+        $user = User::where('username', $username)->first();
+        $perPage = $request->get('per_page', 10);
+
+        $posts = Post::with(['user', 'comments', 'reactions', 'tags'])
+            ->where('user_id', $user->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate($perPage);
 
         return PostResource::collection($posts);
     }
