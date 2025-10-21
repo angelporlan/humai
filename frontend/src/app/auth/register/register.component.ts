59d998647d940181ad882296034d68909886ec4a
angelporlan <angeelporlan4@gmail.com>
2025-10-21 22:05:12 +0200

    Refactor RegisterComponent to improve localStorage handling and code formatting

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -5,10 +5,10 @@ import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
-  imports: [FormsModule, RouterLink],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+    selector: 'app-register',
+    imports: [FormsModule, RouterLink],
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css'
 })
 export class RegisterComponent {
     username = '';
@@ -16,12 +16,16 @@ export class RegisterComponent {
     password = '';
     password_confirmation = '';
 
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(private authService: AuthService, private router: Router) { }
 
     register() {
         this.authService.register(this.username, this.email, this.password, this.password_confirmation).subscribe({
             next: (res) => {
-                localStorage.setItem('token', res.token);
+                const dataRes = {
+                    token: res.token,
+                    username: res.user.username
+                }
+                localStorage.setItem('humai', JSON.stringify(dataRes));
                 this.router.navigate(['/home']);
             },
             error: (err) => console.error(err)
