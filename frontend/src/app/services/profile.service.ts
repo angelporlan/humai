ce77ca801c6e5110ceee7605f973c5acbc9672c2
angelporlan <angeelporlan4@gmail.com>
2025-10-21 22:07:00 +0200

    Add ProfileService with API integration for username info retrieval

---
+++ frontend/src/app/services/profile.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  private apiUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) { }
+
+  getInfoOfAUsername(username: string): Observable<any> {
+    const token = JSON.parse(localStorage.getItem('humai') || '{}').token;
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.get(`${this.apiUrl}/getInfoOfAUsername`, {
+      params: { username },
+      headers: headers
+    });
+  }
+}
