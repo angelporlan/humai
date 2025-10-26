0c113ff9065d13014dd4f67837ee9933a9ac36d3
angelporlan <angeelporlan4@gmail.com>
2025-10-26 19:32:32 +0100

    Add ReactionsComponent with HTML, CSS, and unit tests

---
+++ frontend/src/app/profile/reactions/reactions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactions',
+  imports: [],
+  templateUrl: './reactions.component.html',
+  styleUrl: './reactions.component.css'
+})
+export class ReactionsComponent {
+
+}
