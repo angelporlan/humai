1568b22ffbd47061b308aff7be3ed39944319b8c
angelporlan <angeelporlan4@gmail.com>
2025-10-26 16:37:06 +0100

    Enhance PostResource to include reactionable type and reaction type in response

--- backend/app/Http/Resources/PostResource.php
+++ backend/app/Http/Resources/PostResource.php
@@ -14,6 +14,24 @@ class PostResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        if ($this->reactionable_type) {
+            return [
+                'id' => $this->id,
+                'content' => $this->content,
+                'user' => [
+                    'id' => $this->user->id,
+                    'username' => $this->user->username,
+                    'avatar' => $this->user->avatar,
+                ],
+                'comments_count' => $this->comments_count,
+                'likes_count' => $this->likes_count,
+                'tags' => $this->tags->pluck('name'),
+                'created_at' => $this->created_at->toDateTimeString(),
+                'reactionable_type' => $this->reactionable_type,
+                'reaction_type' => $this->reaction_type,
+            ];
+        }
+
         return [
             'id' => $this->id,
             'content' => $this->content,
