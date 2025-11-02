32a9de1658cd1691c40ed770e8c05367b47cb4f3
angelporlan <angeelporlan4@gmail.com>
2025-11-02 12:49:20 +0100

    Add profile loader component with HTML, CSS, and unit tests

---
+++ frontend/src/app/profile/profile-loader/profile-loader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-loader',
+  imports: [],
+  templateUrl: './profile-loader.component.html',
+  styleUrl: './profile-loader.component.css'
+})
+export class ProfileLoaderComponent {
+
+}
