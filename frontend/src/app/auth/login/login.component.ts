638d98adf609e2d8f16fc742456e5370ab0c1c0f
angelporlan <angeelporlan4@gmail.com>
2025-11-02 12:47:50 +0100

    Add avatar to login response data for user session storage

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -21,7 +21,8 @@ export class LoginComponent {
       next: (res) => {
         const dataRes = {
           token: res.token,
-          username: res.user.username
+          username: res.user.username,
+          avatar: res.user.avatar
         }
         localStorage.setItem('humai', JSON.stringify(dataRes));
         this.router.navigate(['/home']);
