a926ccbe982471e6d0b44581f1f30ead51210ebb
angelporlan <angeelporlan4@gmail.com>
2025-11-07 18:09:37 +0100

    Implement post reaction functionality in FeedItemComponent

--- frontend/src/app/components/feed-item/feed-item.component.ts
+++ frontend/src/app/components/feed-item/feed-item.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { FeedService } from '../../services/feed.service';
 
 @Component({
-  selector: 'app-feed-item',
-  imports: [CommonModule],
-  templateUrl: './feed-item.component.html',
-  styleUrl: './feed-item.component.css'
+    selector: 'app-feed-item',
+    imports: [CommonModule],
+    templateUrl: './feed-item.component.html',
+    styleUrl: './feed-item.component.css'
 })
 export class FeedItemComponent {
     @Input() userAvatar: string = '';
@@ -17,10 +18,21 @@ export class FeedItemComponent {
     @Input() comments: number = 0;
     @Input() tags: string[] = [];
     @Input() reactions: string = '';
+    @Input() postId: number = 0;
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private feedService: FeedService) { }
 
     navigateTo(path: string): void {
         this.router.navigate([path]);
+    }
+
+    reactToAPost(postId: number, type: string): void {
+        this.feedService.reactToAPost(postId, type).subscribe((res) => {
+            if (res.message === 'Reaction removed successfully') {
+                this.likes--;
+            } else {
+                this.likes++;
+            }
+        });
     }
 }
