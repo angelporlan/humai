bc9f0b4ced05853420bf5c8c69f3c1accb9b406c
angelporlan <angeelporlan4@gmail.com>
2025-11-22 10:15:55 +0100

    feat: refactor PeopleItemComponent to include routing and additional user inputs

--- frontend/src/app/home/peoplebox/people-item/people-item.component.ts
+++ frontend/src/app/home/peoplebox/people-item/people-item.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-people-item',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './people-item.component.html',
   styleUrl: './people-item.component.css'
 })
 export class PeopleItemComponent {
+  private router = inject(Router);
+
   @Input() avatar: string = 'default.jpg';
   @Input() username: string = '';
+  @Input() name: string = '';
+  @Input() bio: string = '';
+  @Input() followersCount: number = 0;
+  @Input() mutualFollowers: number = 0;
+  @Input() isFollowing: boolean = false;
+
+  navigateToProfile(): void {
+    this.router.navigate(['/profile', this.username]);
+  }
+
 }
