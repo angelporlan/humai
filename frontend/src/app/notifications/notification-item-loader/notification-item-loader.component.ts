de6660f6811dcdee73f238c00792013fd2e7ae39
angelporlan <angeelporlan4@gmail.com>
2025-11-22 16:47:14 +0100

    feat: add notification item loader component with styles and structure

---
+++ frontend/src/app/notifications/notification-item-loader/notification-item-loader.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-notification-item-loader',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './notification-item-loader.component.html',
+    styleUrls: ['./notification-item-loader.component.css']
+})
+export class NotificationItemLoaderComponent {
+
+}
