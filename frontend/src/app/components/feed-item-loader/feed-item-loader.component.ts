59c78c46229294289775b4ece21f98fbdb5cee67
angelporlan <angeelporlan4@gmail.com>
2025-11-23 19:03:05 +0100

    feat: mark FeedItemLoaderComponent as standalone

--- frontend/src/app/components/feed-item-loader/feed-item-loader.component.ts
+++ frontend/src/app/components/feed-item-loader/feed-item-loader.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-feed-item-loader',
+  standalone: true,
   imports: [],
   templateUrl: './feed-item-loader.component.html',
   styleUrl: './feed-item-loader.component.css'
