53d08a29291762cee198128fd1500d1832831f07
angelporlan <angeelporlan4@gmail.com>
2026-01-27 20:28:54 +0100

    feat: add environment configuration files

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    envMode: 'prod' as 'test' | 'prod',
+
+    backendUrlTest: 'http://127.0.0.1:8000',
+    backendUrlProd: 'https://api-humai.porlan.site',
+
+    get apiUrl(): string {
+        return this.envMode === 'prod' ? this.backendUrlProd : this.backendUrlTest;
+    }
+};
