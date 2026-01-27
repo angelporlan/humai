6aa6d4804fa8f111e317ec1ec0795ccc4cfcce21
angelporlan <angeelporlan4@gmail.com>
2026-01-27 22:51:08 +0100

    refactor: use environment variables in auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = `${environment.apiUrl}/api`;
 
   constructor(private http: HttpClient) { }
 
@@ -30,8 +31,8 @@ export class AuthService {
 
   logout(): Observable<any> {
     const token = JSON.parse(localStorage.getItem('humai') || '{}').token;
-      return this.http.post(`${this.apiUrl}/logout`,
-      {},  
+    return this.http.post(`${this.apiUrl}/logout`,
+      {},
       {
         headers: new HttpHeaders({
           'Authorization': `Bearer ${token}`
