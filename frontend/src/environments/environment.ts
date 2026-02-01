c7eddcf73e60ad674b7d0d2983664a8dac22a76c
angelporlan <angeelporlan4@gmail.com>
2026-02-01 10:39:55 +0100

    refactor: update environment mode to production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    envMode: 'test' as 'test' | 'prod',
+    envMode: 'prod' as 'test' | 'prod',
 
     backendUrlTest: 'http://127.0.0.1:8000',
     backendUrlProd: 'https://api-humai.porlan.site',
